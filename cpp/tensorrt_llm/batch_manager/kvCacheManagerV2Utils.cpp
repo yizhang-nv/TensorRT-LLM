b7ccf1f4c5d74f38d5af053062d25b5517739d49
Yao Yao <lowsfer@users.noreply.github.com>
2025-09-17 10:32:13 +0000

    Finish copy engine
    Signed-off-by: Yao Yao <lowsfer@users.noreply.github.com>

---
+++ cpp/tensorrt_llm/batch_manager/kvCacheManagerV2Utils.cpp
@@ -0,0 +1,140 @@
+#include "tensorrt_llm/batch_manager/kvCacheManagerV2Utils.h"
+#include "tensorrt_llm/common/logger.h"
+#include <cassert>
+#include <cstdio>
+#include <cuda.h>
+#include <fcntl.h>
+#include <filesystem>
+#include <unistd.h>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+namespace tensorrt_llm::batch_manager::kv_cache_manager_v2
+{
+
+// Template to extract the second argument type of a standalone function
+template <typename T>
+struct SecondArgType;
+
+template <typename Ret, typename Arg1, typename Arg2, typename... Rest>
+struct SecondArgType<Ret (*)(Arg1, Arg2, Rest...)>
+{
+    using type = Arg2;
+};
+
+// Helper alias
+template <typename T>
+using SecondArgType_t = typename SecondArgType<T>::type;
+
+template <typename Func>
+bool loopedReadWrite(Func&& func, ssize_t size) noexcept
+{
+    ssize_t count = 0;
+    while (count < size)
+    {
+        ssize_t bytes = func(count);
+        if (bytes <= 0)
+        {
+            if (errno == EINTR)
+            {
+                continue; // Retry on interrupt
+            }
+            return false;
+        }
+        count += bytes;
+    }
+    assert(count == size);
+    return true;
+}
+
+bool writeAll(int fd, ssize_t pos, void const* data, ssize_t size) noexcept
+{
+    return loopedReadWrite([=](ssize_t finished)
+        { return pwrite(fd, static_cast<std::byte const*>(data) + finished, size - finished, pos + finished); },
+        size);
+}
+
+bool readAll(int fd, ssize_t pos, void* data, ssize_t size) noexcept
+{
+    return loopedReadWrite([=](ssize_t finished)
+        { return pread(fd, static_cast<std::byte*>(data) + finished, size - finished, pos + finished); },
+        size);
+}
+
+template <typename DstAddr, typename SrcAddr>
+struct UserData
+{
+    std::vector<Task<DstAddr, SrcAddr>> tasks;
+    ssize_t numBytes;
+};
+
+CUDA_CB void hostFnDiskToDiskCopy(void* userData) noexcept
+{
+    // @TODO: enable multi-threading with a thread pool
+    auto const data = static_cast<UserData<DiskAddress, DiskAddress>*>(userData);
+    std::vector<std::byte> buffer(data->numBytes);
+    bool success = true;
+    for (auto const& t : data->tasks)
+    {
+        success = success && readAll(t.src.fd, t.src.pos, buffer.data(), data->numBytes);
+        success = success && writeAll(t.dst.fd, t.dst.pos, buffer.data(), data->numBytes);
+    }
+    delete data;
+    if (!success)
+    {
+        TLLM_LOG_ERROR("hostFnDiskToDiskCopy failed.\n");
+    }
+}
+
+CUDA_CB void hostFnDiskToHostCopy(void* userData) noexcept
+{
+    // @TODO: enable multi-threading with a thread pool
+    auto const data = static_cast<UserData<MemAddress, DiskAddress>*>(userData);
+    bool success = true;
+    for (auto const& t : data->tasks)
+    {
+        success = success && readAll(t.src.fd, t.src.pos, t.dst, data->numBytes);
+    }
+    delete data;
+    if (!success)
+    {
+        TLLM_LOG_ERROR("hostFnDiskToHostCopy failed.\n");
+    }
+}
+
+CUDA_CB void hostFnHostToDiskCopy(void* userData) noexcept
+{
+    // @TODO: enable multi-threading with a thread pool
+    auto const data = static_cast<UserData<DiskAddress, MemAddress>*>(userData);
+    bool success = true;
+    for (auto const& t : data->tasks)
+    {
+        success = success && writeAll(t.dst.fd, t.dst.pos, t.src, data->numBytes);
+    }
+    delete data;
+    if (!success)
+    {
+        TLLM_LOG_ERROR("hostFnDiskToHostCopy failed.\n");
+    }
+}
+
+CUresult copyDiskToDisk(std::vector<Task<DiskAddress, DiskAddress>> tasks, ssize_t numBytes, CUstream stream) noexcept
+{
+    auto const data = new UserData<DiskAddress, DiskAddress>{std::move(tasks), numBytes};
+    return cuLaunchHostFunc(stream, hostFnDiskToDiskCopy, data);
+}
+
+CUresult copyDiskToHost(std::vector<Task<MemAddress, DiskAddress>> tasks, ssize_t numBytes, CUstream stream) noexcept
+{
+    auto const data = new UserData<MemAddress, DiskAddress>{std::move(tasks), numBytes};
+    return cuLaunchHostFunc(stream, hostFnDiskToHostCopy, data);
+}
+
+CUresult copyHostToDisk(std::vector<Task<DiskAddress, MemAddress>> tasks, ssize_t numBytes, CUstream stream) noexcept
+{
+    auto const data = new UserData<DiskAddress, MemAddress>{std::move(tasks), numBytes};
+    return cuLaunchHostFunc(stream, hostFnHostToDiskCopy, data);
+}
+
+} // namespace tensorrt_llm::batch_manager::kv_cache_manager_v2
--- tensorrt_llm/runtime/kv_cache_manager_v2/_copy_engine.py
+++ tensorrt_llm/runtime/kv_cache_manager_v2/_copy_engine.py
@@ -1,7 +1,18 @@
-import atexit
-from typing import NamedTuple
+import threading
+from collections.abc import Callable, Iterator
+from dataclasses import dataclass
+from functools import cached_property
+from typing import ClassVar, NamedTuple, Sequence
 
-from ._common import Address, CacheTier, CudaStream
+import cuda.bindings.driver as drv
+
+import tensorrt_llm.bindings
+
+from ._common import Address, CacheTier, CudaStream, MemAddress
+from ._utils import (CachedCudaEvent, HomoTuple, HostMem, _unwrap, div_up,
+                     stream_wait_events)
+
+nb_utils = tensorrt_llm.bindings.internal.batch_manager.kv_cache_manager_v2_utils
 
 
 class CopyTask(NamedTuple):
@@ -9,22 +20,227 @@ class CopyTask(NamedTuple):
     src: Address
 
 
-class _CopyEngine:
+def _copy_gpu_to_gpu(tasks: Sequence[CopyTask], num_bytes: int,
+                     stream: CudaStream):
+    # @TODO: use a kernel to do multiple tasks in one batch
+    for dst, src in tasks:
+        _unwrap(drv.cuMemcpyDtoDAsync(dst, src, num_bytes, stream))
 
-    def close(self):
-        pass
 
+def _copy_host_to_host(tasks: Sequence[CopyTask], num_bytes: int,
+                       stream: CudaStream):
+    # @TODO: use a host callback to do multiple tasks in one batch
+    for dst, src in tasks:
+        _unwrap(drv.cuMemcpyHtoHAsync(dst, src, num_bytes, stream))
+
+
+def _copy_disk_to_disk(tasks: Sequence[CopyTask], num_bytes: int,
+                       stream: CudaStream):
+    _unwrap(nb_utils.copy_disk_to_disk(tasks, num_bytes, stream))
+
+
+def _copy_gpu_to_host(tasks: Sequence[CopyTask], num_bytes: int,
+                      stream: CudaStream):
+    # @TODO: use a kernel to do multiple tasks in one batch
+    for dst, src in tasks:
+        _unwrap(drv.cuMemcpyDtoHAsync(dst, src, num_bytes, stream))
+
+
+def _copy_host_to_gpu(tasks: Sequence[CopyTask], num_bytes: int,
+                      stream: CudaStream):
+    # @TODO: use a kernel to do multiple tasks in one batch
+    for dst, src in tasks:
+        _unwrap(drv.cuMemcpyHtoDAsync(dst, src, num_bytes, stream))
+
+
+def _copy_disk_to_host(tasks: Sequence[CopyTask], num_bytes: int,
+                       stream: CudaStream):
+    _unwrap(nb_utils.copy_disk_to_host(tasks, num_bytes, stream))
+
+
+def _copy_host_to_disk(tasks: Sequence[CopyTask], num_bytes: int,
+                       stream: CudaStream):
+    _unwrap(nb_utils.copy_host_to_disk(tasks, num_bytes, stream))
+
+
+Copier = Callable[[Sequence[CopyTask], int, CudaStream], None]
+
+
+def get_copier(dst: CacheTier, src: CacheTier) -> Copier | HomoTuple[Copier]:
+    copiers: HomoTuple[HomoTuple[Copier | HomoTuple[Copier]]] = (
+        # dst = GPU_MEM
+        (
+            _copy_gpu_to_gpu,  # src = GPU_MEM
+            _copy_host_to_gpu,  # src = HOST_MEM
+            (_copy_disk_to_host, _copy_host_to_gpu),  # src = DISK
+        ),
+        # dst = HOST_MEM
+        (
+            _copy_gpu_to_host,  # src = GPU_MEM
+            _copy_host_to_host,  # src = HOST_MEM
+            _copy_disk_to_host,  # src = DISK
+        ),
+        # dst = DISK
+        (
+            (_copy_gpu_to_host, _copy_host_to_disk),  # src = GPU_MEM
+            _copy_host_to_disk,  # src = HOST_MEM
+            _copy_disk_to_disk,  # src = DISK
+        ),
+    )
+    return copiers[dst][src]
+
+
+class StagingBufferManager:
+    __slots__ = ('mutex', 'buffer', 'grains', 'next')
+    GRANULARITY: ClassVar[int] = 1 << 30
+
+    @dataclass(slots=True)
+    class GrainMetadata:
+        mutex: threading.Lock  # protects ready_event.
+        ready_event: CachedCudaEvent  # protects the buffer grain.
+
+    mutex: threading.Lock  # protects next.
+    buffer: HostMem
+    grains: list[GrainMetadata]
+    next: int
+
+    def __init__(self, size: int):
+        assert size % self.GRANULARITY == 0
+        self.mutex = threading.Lock()
+        num_grains = size // self.GRANULARITY
+        self.buffer = HostMem(size)
+        self.grains = [
+            self.GrainMetadata(threading.Lock(), CachedCudaEvent.NULL)
+            for _ in range(num_grains)
+        ]
+        self.next = 0
+
+    @property
+    def size(self) -> int:
+        'Requesting more than this will fail.'
+        assert len(self.grains) * self.GRANULARITY == self.buffer.size
+        return self.buffer.size
+
+    @property
+    def num_grains(self) -> int:
+        return len(self.grains)
+
+    def suggest_next_max_size(self) -> int:
+        'Requesting more than this may degrade performance.'
+        with self.mutex:
+            return self.GRANULARITY * (self.num_grains - self.next)
+
+    # max_size is just a hint, the actual size may be smaller.
+    def new(self, min_size: int, max_size: int,
+            stream: CudaStream) -> 'StagingBufferManager.StagingBuffer':
+        return self.StagingBuffer(self, min_size, max_size, stream)
+
+    class StagingBuffer:
+        __slots__ = ('manager', 'min_size', 'max_size', '_size', 'start_grain',
+                     'stream')
+        manager: "StagingBufferManager"
+        min_size: int
+        max_size: int
+        _size: int
+        start_grain: int
+        stream: CudaStream
+
+        def __init__(self, manager: "StagingBufferManager", min_size: int,
+                     max_size: int, stream: CudaStream):
+            self.manager = manager
+            self.min_size = min_size
+            self.max_size = max_size
+            self.stream = stream
+
+        @property
+        def address(self) -> MemAddress:
+            return MemAddress(self.manager.buffer.address +
+                              self.start_grain * self.manager.GRANULARITY)
+
+        @property
+        def size(self) -> int:
+            return self._size
+
+        @property
+        def num_grains(self) -> int:
+            return div_up(self._size, self.manager.GRANULARITY)
+
+        @property
+        def grains(self) -> list['StagingBufferManager.GrainMetadata']:
+            return self.manager.grains[self.start_grain:self.start_grain +
+                                       self.num_grains]
+
+        def __enter__(self):
+            manager = self.manager
+            if self.min_size > manager.size:
+                raise ValueError(
+                    f"Requested min_size {self.min_size} is too large for the manager"
+                )
+            self._size = min(self.max_size, manager.suggest_next_max_size())
+            with manager.mutex:
+                self.start_grain = manager.next
+                manager.next += self.num_grains
+                assert manager.next <= manager.num_grains
+                if manager.next == manager.num_grains:
+                    manager.next = 0
+
+                def lock_and_consume_events() -> Iterator[CachedCudaEvent]:
+                    for grain in self.grains:
+                        grain.mutex.acquire()
+                        yield grain.ready_event
+
+                stream_wait_events(self.stream, lock_and_consume_events())
+                return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            event = CachedCudaEvent(self.stream)
+            for grain in reversed(self.grains):
+                grain.ready_event = event
+                grain.mutex.release()
+
+
+class CopyEngine:
+    __slots__ = ()
+
+    # use cached_property so it's created only on first access, when cuda context has been initialized.
+    @cached_property
+    def staging_buffer_manager(self) -> StagingBufferManager:
+        return StagingBufferManager(64 << 20)
+
+    # @TODO: Use a dedicated stream for each different Copier, take set[CachedCudaEvent] instead of stream, and return a new CachedCudaEvent.
     def transfer(self, dst_cache_tier: CacheTier, src_cache_tier: CacheTier,
-                 num_bytes: int, tasks: list[CopyTask], stream: CudaStream):
-        pass
+                 num_bytes: int, tasks: Sequence[CopyTask],
+                 stream: CudaStream) -> None:
+        copier = get_copier(dst_cache_tier, src_cache_tier)
+        if not isinstance(copier, tuple):
+            return copier(tasks, num_bytes, stream)
+        assert len(
+            copier) == 2  # for now, we only support 2 copiers via host memory
+        manager = self.staging_buffer_manager
+        remaining = tasks
+        while remaining:
+            with manager.new(num_bytes, num_bytes * len(remaining),
+                             stream) as buf:
+                addr = buf.address
+                n = buf.size // num_bytes
+                assert n <= len(remaining)
+                batch = remaining[:n]
+                copier[0]([
+                    CopyTask(MemAddress(addr + num_bytes * i), t.src)
+                    for i, t in enumerate(batch)
+                ], num_bytes, buf.stream)
+                copier[1]([
+                    CopyTask(t.dst, MemAddress(addr + num_bytes * i))
+                    for i, t in enumerate(batch)
+                ], num_bytes, buf.stream)
+                remaining = remaining[n:]
 
 
-_copy_engine = _CopyEngine()
-atexit.register(_copy_engine.close)
+_copy_engine = CopyEngine()
 
 
 def batched_copy(dst_cache_tier: CacheTier, src_cache_tier: CacheTier,
-                 num_bytes: int, tasks: list[CopyTask],
+                 num_bytes: int, tasks: Sequence[CopyTask],
                  stream: CudaStream) -> None:
     _copy_engine.transfer(dst_cache_tier, src_cache_tier, num_bytes, tasks,
                           stream)
